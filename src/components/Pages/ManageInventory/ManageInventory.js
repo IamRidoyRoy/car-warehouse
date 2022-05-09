87c5e8153dfc5cd7b5f1b8417d836f6bb1519db8
Ridoy Roy <ridoyroycse53engg@gmail.com>
2022-05-09 06:21:49 +0600

    Add and implememt delete button in manage inventory

--- src/components/Pages/ManageInventory/ManageInventory.js
+++ src/components/Pages/ManageInventory/ManageInventory.js
@@ -1,9 +1,43 @@
-import React from 'react';
+import { useEffect, useState } from "react";
 
 const ManageInventory = () => {
+    const [caritems, setCarItems] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/car')
+            .then(res => res.json())
+            .then(data => setCarItems(data))
+    }, []);
+    const handleDeleteButton = id => {
+        const proceed = window.confirm("Are you sure to delete?")
+        if (proceed) {
+            console.log("Post is deleting");
+            const url = `http://localhost:5000/inventory/${id}`
+            fetch(url, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data)
+                    const remaining = caritems.filter(car => car._id !== id);
+                    setCarItems(remaining)
+                })
+        }
+    }
+
     return (
-        <div>
-            <h2>This is from inventory management system</h2>
+        <div className='w-50 mx-auto text-center mt-5'>
+            <button>Add New Item</button>
+            <h2>This is from Manage user</h2>
+            <div>
+                {
+                    caritems.map(car => <div key={car._id}>
+
+                        <h5>Name : {car.name} <button onClick={() => handleDeleteButton(car._id)}>Delete</button> </h5>
+
+                    </div>)
+                }
+            </div>
         </div>
     );
 };
